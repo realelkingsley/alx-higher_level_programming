688b31e077dc763162e2b5c18ff261d34eb96de9
realelkingsley <kc198070@gmail.com>
2023-01-20 17:16:55 +0100

    ike

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""writes an object to a text file, using a JSON representation"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Args
+        my_obj (dict): object to be converted to a JSON object
+[I        filename (file pointer): pointer to json file
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
+
